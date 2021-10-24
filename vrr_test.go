0c1c6f957de931da7f7dcbf891178266b1cbeb6f
joshuabezaleel <joshua.bezaleel@gmail.com>
2021-10-24 17:20:59 +0700

    Tidy up test

--- vrr_test.go
+++ vrr_test.go
@@ -34,20 +34,25 @@ func TestViewChangePrimaryDisconnect(t *testing.T) {
 func TestViewChangePrimaryDisconnectThenReconnect(t *testing.T) {
 	h := NewHarness(t, 3)
 	defer h.Shutdown()
-	origPrimaryID, _ := h.CheckSinglePrimary()
+	origPrimaryID, origView := h.CheckSinglePrimary()
 
 	h.DisconnectPeer(origPrimaryID)
 
 	sleepMs(350)
 	newPrimaryID, newView := h.CheckSinglePrimary()
 
-	t.Logf("YOW %v %v", newPrimaryID, newView)
-
 	h.ReconnectPeer(origPrimaryID)
 	sleepMs(150)
 
 	newPrimaryID, newView = h.CheckSinglePrimary()
-	t.Logf("YOW %v %v", newPrimaryID, newView)
+	if newPrimaryID == origPrimaryID {
+		t.Errorf("want new primary to be different from orig primary, got %d and %d", newPrimaryID, origPrimaryID)
+	}
+	if newView <= origView {
+		t.Errorf("want newView > origView, got %d and %d", newView, origView)
+	}
 
+	// Replica[0] should have viewNum of 1 and primaryID 1
+	// from State Transfer.
 	h.ReportAll()
 }
